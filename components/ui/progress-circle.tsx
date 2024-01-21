8a9ae709fbab39bbae5b5a85b427108384967a0e
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2024-01-21 14:38:05 +0530

    user betting UI

---
+++ components/ui/progress-circle.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface ProgressIndicatorProps {
+  completion: number; // expects a value between 0 and 100
+}
+
+const ProgressIndicator: React.FC<ProgressIndicatorProps> = ({
+  completion,
+}) => {
+  const radius = 50; // radius of the circle
+  const stroke = 10; // width of the stroke
+  const normalizedRadius = radius - stroke * 2;
+  const circumference = normalizedRadius * 2 * Math.PI;
+  const strokeDashoffset = circumference - (completion / 100) * circumference;
+
+  return (
+    <div className='flex items-center justify-center'>
+      <svg
+        height={radius * 2}
+        width={radius * 2}
+        className='transform -rotate-90'
+      >
+        <circle
+          stroke='blue'
+          fill='transparent'
+          strokeWidth={stroke}
+          strokeDasharray={circumference + " " + circumference}
+          style={{ strokeDashoffset }}
+          r={normalizedRadius}
+          cx={radius}
+          cy={radius}
+          className='transition-all duration-350 ease-linear'
+        />
+      </svg>
+      <span className='absolute text-lg font-extrabold text-blue-700 font-mono'>
+        {completion}%
+      </span>
+    </div>
+  );
+};
+
+export default ProgressIndicator;
--- pages/BettingHome.tsx
+++ pages/BettingHome.tsx
@@ -1,3 +1,7 @@
+import { Label } from "@/components/ui/label";
+import ProgressIndicator from "@/components/ui/progress-circle";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { BadgeDollarSign, BadgeEuro } from "lucide-react";
 import React from "react";
 
 function BettingHome() {
@@ -19,7 +23,7 @@ function BettingHome() {
             </h1>
           </div>
         </div>
-        <div className='bg-[#DBFF4A] px-8 py-10 rounded-2xl'>
+        <div className='bg-white px-8 py-10 rounded-2xl'>
           <div className='flex items-center'>
             <button className='text-white bg-black rounded-full py-3 px-6'>
               New Bets
@@ -30,6 +34,80 @@ function BettingHome() {
             <button className='text-black bg-transparent border border-black rounded-full py-3 px-6 ml-5'>
               Finished Bets
             </button>
+          </div>
+
+          {/* grid bet card results */}
+          <div className='grid grid-cols-1 gap-8 lg:grid-cols-3 mt-5 text-black'>
+            <article className='relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8'>
+              <p className='mt-6 flex gap-x-2 text-sm text-neutral-950'>
+                <time className='font-semibold text-[#322EFF] hover:underline underline-offset-4 cursor-pointer'>
+                  @matt03
+                </time>
+                <span className='text-neutral-300' aria-hidden='true'>
+                  /
+                </span>
+                <span>2 hrs ago</span>
+              </p>
+              <p className='mt-6 font-display text-2xl font-semibold text-neutral-800'>
+                Will trump win the 2024 Election?
+              </p>
+              <div className='flex mt-4 text-sm font-mono'>
+                <p className='px-2 py-1 bg-black/80 text-white mr-2'>
+                  politics
+                </p>
+                <p className='px-2 py-1 bg-black/80 text-white mr-2'>
+                  election
+                </p>
+                <p className='px-2 py-1 bg-black/80 text-white mr-2'>US</p>
+              </div>
+              <ProgressIndicator completion={70} />
+              <p className='mx-auto text-sm font-bold text-neutral-600'>
+                chance of happening
+              </p>
+
+              {/* place bet */}
+
+              <h3 className='text-lg font-semibold text-gray-800 tracking-tight mt-10 font-open-sans'>
+                Place your bet
+              </h3>
+
+              <RadioGroup
+                defaultValue='will-happen'
+                className='mt-5 [&_label]:text-lg [&_label:hover]:underline [&_label:hover]:cursor-pointer'
+              >
+                <div className='flex items-center space-x-2'>
+                  <RadioGroupItem value='will-happen' id='will-happen' />
+                  <Label htmlFor='will-happen'>Bet Yes</Label>
+                </div>
+                <div className='flex items-center space-x-2'>
+                  <RadioGroupItem
+                    value='will-not-happen'
+                    id='will-not-happen'
+                  />
+                  <Label htmlFor='will-not-happen'>Bet No</Label>
+                </div>
+              </RadioGroup>
+              <div className='flex mt-4 items-center'>
+                <div className='flex'>
+                  <div className='h-10 w-10 bg-black border-2 border-neutral-900/20  rounded-full shadow-sm z-[2] flex items-center justify-center'>
+                    <BadgeEuro className='text-white h-6 w-6' />
+                  </div>
+                  <div className='h-10 w-10  bg-gray-800/20 border-neutral-900/20 border-2 rounded-full -ml-4 shadow-sm z-[1] flex items-center justify-center'>
+                    <BadgeDollarSign className='text-gray-800/40 h-6 w-6' />
+                  </div>
+                </div>
+                <input
+                  type='number'
+                  className='outline-none text-right min-w-0 text-lg font-semibold tracking-tight bg-transparent placeholder:text-black/30'
+                  placeholder='20,000'
+                />
+                <h4 className='text-md font-semibold ml-1'>Coins.</h4>
+              </div>
+
+              <button className='text-white bg-black rounded-lg text-sm py-2 px-4 self-start ml-auto'>
+                PLACE BET
+              </button>
+            </article>
           </div>
         </div>
       </div>
